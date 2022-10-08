a4ab515091da5938f2373968dd7d7b2e428e1ada
Felipe Kessi <kessi@koders.cl>
2022-10-08 13:02:24 -0300

    actualizacion clase

--- assets/JS/claseApi.js
+++ assets/JS/claseApi.js
@@ -1,7 +1,38 @@
 $(".btn").on("click", function() {
-    alert($(this).text('Loading...'));
+    alert("Trayendo objeto");
+    $(this).text('Loading...');
     $.ajax({
         type: 'GET',
-        url: "https://api.meetup.com/2/cities"
-    })
+        url: "https://reqres.in/api/users",
+        dataType: 'json',
+        success: function(data) {
+            $('.text').text(JSON.stringify(data));
+        },
+        error: function(error) {
+            alert("Error");
+        }
+    });
+});
+
+
+jQuery.fn.datosFinancieros = function() {
+    var element = this;
+    $.ajax({
+        type: "GET",
+        url: "https://mindicador.cl/api",
+        dataType: "json",
+        success: function(data) {
+            element.append(
+                `<span>${data.uf.nombre}</span>
+                <span>${data.uf.valor}</span>`
+
+            )
+        }
+    });
+    return this;
+};
+
+
+$(function() {
+    $("#uf").datosFinancieros();
 });
