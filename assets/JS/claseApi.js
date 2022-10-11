504f3c9c81da2e93843497d4bcce93241305d6a9
Felipe Kessi <kessi@koders.cl>
2022-10-11 20:06:35 -0300

    actualizacion clase

--- assets/JS/claseApi.js
+++ assets/JS/claseApi.js
@@ -1,10 +1,12 @@
+// llamada API reqres en el boton apretame
 $(".btn").on("click", function() {
-    alert("Trayendo objeto");
+    alert("Después de apretar el Boton traré los datos de la API");
     $(this).text('Loading...');
+    //Aquí comienza el llamado a la API
     $.ajax({
-        type: 'GET',
-        url: "https://reqres.in/api/users",
-        dataType: 'json',
+        type: 'GET', //tipo de llamado pedir información
+        url: "https://reqres.in/api/users", //url de la API
+        dataType: 'json', //tipo de datos que se espera recibir
         success: function(data) {
             $('.text').text(JSON.stringify(data));
         },
@@ -14,18 +16,17 @@ $(".btn").on("click", function() {
     });
 });
 
-
+//Llamada a la API de datos financieros
 jQuery.fn.datosFinancieros = function() {
-    var element = this;
-    $.ajax({
+    var element = this; //elemento que se esta llamando
+    $.ajax({ //Aqui parte el llamado a la API
         type: "GET",
-        url: "https://mindicador.cl/api",
-        dataType: "json",
+        url: "https://mindicador.cl/api", //url de la API
+        dataType: "json", //tipo de datos que se espera recibir
         success: function(data) {
             element.append(
                 `<span>${data.uf.nombre}</span>
                 <span>${data.uf.valor}</span>`
-
             )
         }
     });
